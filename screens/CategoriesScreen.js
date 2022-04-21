a8b52b479b24da0e44c1de1d9d481cb5690e91ba
Derek Phavorachit <derek.phavorachit@gmail.com>
2022-04-21 14:56:45 -0500

    Fixed item -> itemData to extract data from categories

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,9 +1,9 @@
 import { FlatList } from 'react-native';
 import CategoryGridTile from '../components/CategoryGridTile';
 
-import { CATEGORIES } from '../data/dummy-dat';
+import { CATEGORIES } from '../data/dummy-data';
 
-function renderCategoryItem(item) {
+function renderCategoryItem(itemData) {
     return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />;
 }
 
