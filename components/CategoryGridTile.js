3abffd36e25282639f1d477f89ec574990525e1e
Derek Phavorachit <derek.phavorachit@gmail.com>
2022-04-25 09:49:17 -0500

    Add ripple/press effect for both Android and IOS

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,12 +1,12 @@
-import { Pressable, View, Text, StyleSheet } from 'react-native';
+import { Pressable, View, Text, StyleSheet, Platform } from 'react-native';
 import { shadowColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
 function CategoryGridTile({title, color}) {
     return (
     <View style={styles.gridItem}>
-        <Pressable style={styles.button}>
+        <Pressable android_ripple={{color: '#ccc'}} style={({pressed}) => [styles.button, pressed ? styles.buttonPressed : null]}>
             <View style={styles.innerContainer}>
-                <Text>{title}</Text>
+                <Text style={styles.title}>{title}</Text>
             </View>
         </Pressable>
     </View>
@@ -28,15 +28,23 @@ const styles = StyleSheet.create({
         shadowColor: 'black',
         shadowOpacity: 0.25,
         shadowOffset: { width: 0, height: 2},
-        shadowRadius: 8
+        shadowRadius: 8,
+        overflow: Platform.OS === 'android' ? 'hidden' : 'visible',     //hides overflow beyond the rounded corners for ripple
     },
     button: {
         flex: 1
+    },
+    buttonPressed: {
+        opacity: 0.5
     },
     innerContainer: {
         flex: 1,
         padding: 16,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18
     }
 });
