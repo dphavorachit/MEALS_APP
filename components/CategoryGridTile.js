471d9b3b09d7fd61974ccbe544359f117b60d030
Derek Phavorachit <derek.phavorachit@gmail.com>
2022-04-25 09:41:44 -0500

    Add inital styling to category buttons

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,10 +1,11 @@
-import { Pressable, View, Text } from 'react-native';
+import { Pressable, View, Text, StyleSheet } from 'react-native';
+import { shadowColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
 function CategoryGridTile({title, color}) {
     return (
-    <View>
-        <Pressable>
-            <View>
+    <View style={styles.gridItem}>
+        <Pressable style={styles.button}>
+            <View style={styles.innerContainer}>
                 <Text>{title}</Text>
             </View>
         </Pressable>
@@ -13,3 +14,29 @@ function CategoryGridTile({title, color}) {
 }
 
 export default CategoryGridTile;
+
+const styles = StyleSheet.create({
+    gridItem: {
+        flex: 1,
+        margin: 16,
+        height: 150,
+        borderRadius: 8,
+        // Android
+        elevation: 4,
+        // IOS
+        backgroundColor: 'white',
+        shadowColor: 'black',
+        shadowOpacity: 0.25,
+        shadowOffset: { width: 0, height: 2},
+        shadowRadius: 8
+    },
+    button: {
+        flex: 1
+    },
+    innerContainer: {
+        flex: 1,
+        padding: 16,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
