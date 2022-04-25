99a74357c5e110ef6c381b634cb8f1f9f4660e6f
Derek Phavorachit <derek.phavorachit@gmail.com>
2022-04-25 10:16:19 -0500

    Add Stack navigator from react navigation. Add categories screen to the stack.

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import CategoriesScreen from './screens/CategoriesScreen';
+
+// Stack now contains a stack of screens that holds 2 component: name, component.
+// The name is a way to give an ID to the screen. Component points to the screen component. Just the pointerk.
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
     <StatusBar style='light' />
     <NavigationContainer>
-       <CategoriesScreen />
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        </Stack.Navigator>
+        {/*<CategoriesScreen /> // Not needed anymore since the screen is in the stack*/}
     </NavigationContainer>
     </>
     
