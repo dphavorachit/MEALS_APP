ec3b63a541c4e2e452aae7479e2311f2ba50c944
Derek Phavorachit <derek.phavorachit@gmail.com>
2022-04-25 11:06:35 -0500

    Sort/filter different categories of meals by item data and category id.

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,17 +1,33 @@
 
-import { View, Text, StyleSheet } from 'react-native';
+import { render } from 'react-dom';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+// Can also use hook as an alternative to using route
+// import { useRoute } from '@react-navigation/native';
+
+import MealItem from '../components/MealItem';
 import { MEALS } from '../data/dummy-data';
 
 // Route prop can be used like the navigation prop.
 function MealsOverviewScreen({ route }) {
     // optional object containing params which is defined while navigating. 
     const catId = route.params.categoryId;  // comes from the CategoriesScreen. the param "CategoryId" is defined in the navigation prop for MealsOverview
+   
+    // Using useRoute hook instead of route prop
+    // const route = useRoute();
+    // route.params...
+
+    const displayedMeals = MEALS.filter((mealItem) => { 
+        return mealItem.categoryIds.indexOf(catId) >= 0;
+     });
+
+    // Imported from MealItem.js as a function
+     function renderMealItem(itemData) {
+         return <MealItem title={itemData.item.title} />;
+     };
 
     return (
         <View style={styles.container}>
-            <Text>
-                Meals Overview Screen - {catId}
-            </Text>
+            <FlatList data={displayedMeals} keyExtractor={(item) => item.id} renderItem={renderMealItem} />
         </View>
     )
 };
