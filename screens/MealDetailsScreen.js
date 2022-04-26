0e5f143cda07165737b9658a8f5f82a9bc762c7d
Derek Phavorachit <derek.phavorachit@gmail.com>
2022-04-26 10:13:14 -0500

    Style Ingredients and Steps subtitles.

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,4 +1,4 @@
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, StyleSheet } from 'react-native';
 
 import MealDetails from '../components/MealDetails';
 import { MEALS } from '../data/dummy-data';
@@ -9,19 +9,55 @@ function MealDetailsScreen({route}) {
     const selectedMeal = MEALS.find((meal) => meal.id === mealId);      // gives access to MEALS properties
     return (
         <View>
-            <Image source={{ uri: selectedMeal.imageUrl }}/>
-            <Text>{selectedMeal.title}</Text>
+            <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }}/>
+            <Text style={styles.title}>{selectedMeal.title}</Text>
             <MealDetails 
                 duration={selectedMeal.duration} 
                 complexity={selectedMeal.complexity} 
-                affordability={selectedMeal.affordability} 
+                affordability={selectedMeal.affordability}
+                textStyle={styles.detailText}
             />
-            <Text>Ingredients</Text>
+            <View style={styles.subtitleContainer}>
+                <Text style={styles.subtitle}>Ingredients</Text>
+            </View>
             {selectedMeal.ingredients.map((ingredient) => (<Text key={ingredient}>{ingredient}</Text>))}
-            <Text>Steps</Text>
+            <View style={styles.subtitleContainer}>
+                <Text style={styles.subtitle}>Steps</Text>
+            </View>
             {selectedMeal.steps.map((step) => (<Text key={step}>{step}</Text>))}
         </View>
     )
 }
 
 export default MealDetailsScreen;
+
+const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: 350
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 24,
+        margin: 8,
+        textAlign: 'center',
+        color: 'white'
+    },
+    detailText: {
+        color: 'white'
+    },
+    subtitle: {
+        color: '#e2b497',
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        
+    },
+    subtitleContainer: {
+        marginHorizontal: 24,
+        marginVertical: 4,
+        padding: 6,
+        borderBottomColor: '#e2b497',
+        borderBottomWidth: 2
+    }
+});
